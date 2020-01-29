a887318b715f69ac6f6ddb6a7bae89480f8d4977
Nick Solonyy <nicksolony@gmail.com>
2020-01-29 05:12:49 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -1,7 +1,7 @@
 def selects_all_female_bears_return_name_and_age
   "select name,age
   from bears
-  where gender = "F""
+  where gender = 'F'"
 end
 
 def selects_all_bears_names_and_orders_in_alphabetical_order
