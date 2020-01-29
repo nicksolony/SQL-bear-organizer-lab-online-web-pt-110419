188f8bc875cb3588cd41bccd7cff46a12ab94e43
Nick Solonyy <nicksolony@gmail.com>
2020-01-29 05:21:02 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -30,7 +30,7 @@ end
 def selects_most_prominent_color_and_returns_with_count
   "SELECT color, COUNT(color)
   FROM bears
-  where color = ;"
+  where color = 'dark brown';"
 end
 
 def counts_number_of_bears_with_goofy_temperaments
