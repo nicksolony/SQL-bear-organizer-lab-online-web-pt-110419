e8df11a5407acda9bdd69eb8fbf0ad2904a81800
Nick Solonyy <nicksolony@gmail.com>
2020-01-29 05:10:08 +0000

    Automatically backed up by Learn

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age,gender, color,temperament, alive) VALUES ("Mr. Chocolate", 10, "Male", "Brown","Kind",1);
+INSERT INTO bears (name, age,gender, color,temperament, alive) VALUES ("Rowdy", 7, "Male", "Black","Friendly",1);
+INSERT INTO bears (name, age,gender, color,temperament, alive) VALUES ("Tabitha", 13, "Female", "Brown","Aggresive",0);
+INSERT INTO bears (name, age,gender, color,temperament, alive) VALUES ("Sergeant Brown", 8, "Male", "Brown","Aggresive",1);
+INSERT INTO bears (name, age,gender, color,temperament, alive) VALUES ("Melissa", 6, "Female", "Black","Kind",1);
+INSERT INTO bears (name, age,gender, color,temperament, alive) VALUES ("Grinch", 4, "Male", "White","Bad",1);
+INSERT INTO bears (name, age,gender, color,temperament, alive) VALUES ("Wendy", 5, "Female", "Blacke","Funny",1);
+INSERT INTO bears (temperament, alive) VALUES ("Aggresive",1);
